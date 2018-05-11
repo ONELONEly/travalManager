806302fc1c2b65228ac379ccbb24b712c2df4f0d
ONELONEly <1825326327@qq.com>
2018-05-11 21:37:11 +0800

    将审批管理写好的页面部分复用到订单管理，企业管理，个人中心中

--- src/components/Buy/Buy.vue
+++ src/components/Buy/Buy.vue
@@ -1,12 +1,6 @@
 <template>
-  <div id="buy">
-    <mu-breadcrumb class="buy_breadcrumb">
-      <mu-breadcrumb-item href="/">
-        <mu-icon value="home" :size="16"/>
-        首页
-      </mu-breadcrumb-item>
-      <mu-breadcrumb-item href="/">出行预定</mu-breadcrumb-item>
-    </mu-breadcrumb>
+  <div >
+出行预定
   </div>
 </template>
 
@@ -17,15 +11,5 @@
 </script>
 
 <style lang="scss">
-  #buy {
-    .buy_breadcrumb {
-      margin: 30px 50px;
-      width: 100%;
-      .mu-breadcrumb-item-link {
-        color: white;
-        margin: 10px;
-      }
-    }
 
-  }
 </style>
