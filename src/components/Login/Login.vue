898e0c49362c9ad2620110256952d5147af89288
180023 <gree180023>
2018-05-12 15:24:18 +0800

    登录编写

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -11,11 +11,12 @@
       <!--用户登陆文字-->
       <p class="login_user_label">用户登陆</p>
       <!--用户名-->
-      <mu-text-field label="用户名" labelFloat/><br/>
+      <mu-text-field v-model="username" label="用户名" :errorText="usernameError" hintText="请输入用户名" :maxLength = "11" labelFloat /><br/>
       <!--密码-->
-      <mu-text-field label="密码" hintText="请输入密码" type="password" labelFloat/>
+      <mu-text-field v-model="password" label="密码" :errorText="passwordError" hintText="请输入密码" :maxLength = "20" type="password" labelFloat/>
       <!--登陆按钮-->
       <mu-float-button icon="arrow_forward" class="login_submit_btn" @click="onLogin"/>
+      <p class="response_message"><span>{{msg}}</span></p>
     </mu-paper>
   </div>
 </template>
@@ -26,12 +27,55 @@ export default {
   // 数据
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: '',
+      username: '',
+      password:'',
+      usernameError:'',
+      passwordError:''
     }
   },
   methods: {
     onLogin() {
-      this.$router.push('/home')
+      this.usernameError = '';
+      this.passwordError = '';
+      if(this.inputValidation()) {
+        let postData = this.$qs.stringify({
+          username: this.username,
+          password: this.password
+        });
+        this.$http.post("/cmx2/loginCheck", postData).then(function (res) {
+          if (res.data.data) {
+            this.msg = '登陆成功';
+//            this.$router.push('/home');
+          } else {
+            this.msg = "密码错误";
+          }
+        }.bind(this));
+
+        this.$http.post("/cmx2/user/queryAlwaysUser").then(function (res) {
+          console.log(res.data);
+        }.bind(this));
+
+      }
+    },
+    inputValidation() {
+
+      if(this.username.length < 1 || this.username.indexOf(' ') > -1 || this.username.length > 11){
+        this.usernameError = "请输入用户名（不包含空格，且在11个字符之内）";
+        return false;
+      }else{
+        this.usernameError = '';
+      }
+
+      if(this.password.length < 1 || this.password.indexOf(' ') > -1 || this.password.length > 20){
+        this.passwordError = "请输入密码（不包含空格，且在20个字符之内）";
+        return false;
+      }else{
+        this.passwordError = '';
+      }
+
+      return true;
+
     }
   }
 }
@@ -80,6 +124,12 @@ export default {
         font-size: 1.6rem;
         color: #03a9f4;
       }
+     .response_message {
+       margin: 0;
+       padding: 0;
+       font-size: 1.0rem;
+       color: #f00;
+     }
       // 登陆按钮
       .login_submit_btn {
         position: absolute;
